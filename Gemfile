1e8811a7cebf843b8a6af39d5063cf3425e4a833
Kayla Comalli <Kayla.comalli@gmail.com>
2019-11-06 13:50:03 +0100

    swap sqlite w pg

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-ruby '~> 2.6.5'
+ruby '~> 2.6.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '6.0.0'
@@ -32,7 +32,6 @@ end
 
 group :development, :test do
   gem 'puma'
-  gem 'sqlite3'
 end
 
 group :production do
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2017_06_13_183247) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "activities", force: :cascade do |t|
     t.string "trackable_type"
     t.integer "trackable_id"
