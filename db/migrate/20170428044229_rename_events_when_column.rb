6c03eab1c1649e57b8f99ada2cce55dbbfe4692b
Kayla Comalli <Kayla.comalli@gmail.com>
2019-11-12 19:09:09 +0100

    trying again w version 5.1

--- db/migrate/20170428044229_rename_events_when_column.rb
+++ db/migrate/20170428044229_rename_events_when_column.rb
@@ -1,4 +1,4 @@
-class RenameEventsWhenColumn < ActiveRecord::Migration[5.0]
+class RenameEventsWhenColumn < ActiveRecord::Migration[5.1][5.0]
   def change
     rename_column :events, :when, :event_datetime
   end
